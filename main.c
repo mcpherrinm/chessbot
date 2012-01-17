40ed38e3685d9460d46c4f27a20d26c64e546c44
John Ladan <jladan@strombola.csclub.uwaterloo.ca>
2012-01-17 15:12:09 -0500

    now uses relative angles for base coordinates, and new metric

--- main.c
+++ main.c
@@ -7,36 +7,65 @@
 
 float p[4][2] = { 250.0, 100.0, 0,0,0,0,0,0 };
 float theta[4] = { 0.3, 0.6, M_PI, 0.0 };
-float length[3] = {234.0, 300.0, 60.0};
+float rtheta[4] = { 0.3, 0.6, M_PI, 0.0 }; // (relative angles) make this match the theta values better
+float length[3] = {234.0/1.5, 300.0/1.5, 60.0/1.5};
 int grip = 0;
 
-#define DT 0.008
-#define WIDTH 1290
-#define HEIGHT 720
+#define DT 0.05
+#define WIDTH 640
+#define HEIGHT 480
 #define DELAY 0.01
 #define SPEED 1000
 
+#define GRIP_BIAS 10.0
+
 float min(float a, float b) { return a > b ? b : a; }
+
+// distance from the arm to the mouse
+float distance(float rtheta0, float rtheta1, float rtheta2, float x0, float y0) {
+    // get absolute angles from relative
+    float theta0 = rtheta0;
+    float theta1 = rtheta1 - (M_PI - theta0);
+    float theta2 = rtheta2 - (M_PI - theta1);
+
+    // x and y values for tip of gripper
+    float x = cos(theta0)*length[0] + cos(theta1)*length[1] + cos(theta2)*length[2];
+    float y = sin(theta0)*length[0] + sin(theta1)*length[1] + sin(theta2)*length[2];
+
+    // the actual measure
+    return pow(x-x0,2) + pow(y-y0,2) + GRIP_BIAS*pow(theta2 + M_PI/2,2);
+    // L1 measure
+    //return fabs(x-x0) + fabs(y-y0) + GRIP_BIAS*fabs(theta2 + M_PI/2);
+}
+
 
 void solve(float tx, float ty) {
   float minerror = 10000000.0;
-  for (float x = -M_PI/2.0; x < M_PI/2.0; x += DT) {
-    for (float y = x; y < min(M_PI, x + 13.0*M_PI/16.0); y+=DT) {
-      float error = fabs(tx - (p[0][0] + sin(x)*length[0] + sin(y)*length[1] + sin(theta[2])*length[2]))
-                  + fabs(ty - (p[0][1] + cos(x)*length[0] + cos(y)*length[1] + cos(theta[2])*length[2]));
-      if (error < minerror) {
-        minerror = error;
-        theta[0] = x;
-        theta[1] = y;
+  for (float rt0 = 0; rt0 < M_PI; rt0 += DT) {
+    for (float rt1 = M_PI/16; rt1 < M_PI; rt1 += DT) {
+      for (float rt2 = M_PI/16; rt2 < M_PI; rt2 += DT) {
+        float error = distance(rt0, rt1, rt2, tx, ty);
+        if (error < minerror) {
+          minerror = error;
+          rtheta[0] = rt0;
+          rtheta[1] = rt1;
+          rtheta[2] = rt2;
+        }
       }
     }
   }
-  p[1][0] = p[0][0] + sin(theta[0])*length[0];
-  p[1][1] = p[0][1] + cos(theta[0])*length[0];
-  p[2][0] = p[1][0] + sin(theta[1])*length[1];
-  p[2][1] = p[1][1] + cos(theta[1])*length[1];
-  p[3][0] = p[2][0] + sin(theta[2])*length[2];
-  p[3][1] = p[2][1] + cos(theta[2])*length[2];
+
+  // I should really make this coordinate change into a function
+  theta[0] = rtheta[0];
+  theta[1] = rtheta[1] - (M_PI - theta[0]);
+  theta[2] = rtheta[2] - (M_PI - theta[1]);
+
+  p[1][0] = p[0][0] + cos(theta[0])*length[0];
+  p[1][1] = p[0][1] + sin(theta[0])*length[0];
+  p[2][0] = p[1][0] + cos(theta[1])*length[1];
+  p[2][1] = p[1][1] + sin(theta[1])*length[1];
+  p[3][0] = p[2][0] + cos(theta[2])*length[2];
+  p[3][1] = p[2][1] + sin(theta[2])*length[2];
 
 }
 
@@ -80,14 +109,18 @@ int main() {
     int x, y;
     glfwGetMousePos(&x, &y);
     y = HEIGHT - y;
-    theta[3] = (double)y/(double)HEIGHT*2.0-1.0;
-    solve(x, p[0][1]+20);
-    //solve(x, y);
+    x-= p[0][0];
+    y-= p[0][1];
+    //theta[3] = (double)y/(double)HEIGHT*2.0-1.0;
+    //solve(x, p[0][1]+20);
+    solve(x, y);
     double t = glfwGetTime();
     if (t - t0 > DELAY) {
       grip = !glfwGetMouseButton(GLFW_MOUSE_BUTTON_1);
       t0 = t;
-      sayshit();
+      // sayshit();
+      printf("%f, %f, %f, %f\n", rtheta[0]*180/M_PI,rtheta[1]*180/M_PI,rtheta[2]*180/M_PI,theta[2]*180/M_PI);
+
     }
 
     glClear(GL_COLOR_BUFFER_BIT);
